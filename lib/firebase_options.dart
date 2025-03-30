f215f6de3a2b2c58a9db078cbafca9bd53b1e83c
Ig-BabyBeast <165694645+Ig-BabyBeast@users.noreply.github.com>
2025-03-30 23:24:38 +0530

    Add lib/firebase_options.dart 1743357259948

---
+++ lib/firebase_options.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError(
+        'Web platform is not supported for this application',
+      );
+    }
+
+    // TODO: Replace with your actual Firebase configuration
+    return FirebaseOptions(
+      apiKey: 'YOUR_API_KEY',
+      appId: 'YOUR_APP_ID',
+      messagingSenderId: 'YOUR_SENDER_ID',
+      projectId: 'YOUR_PROJECT_ID',
+      storageBucket: 'YOUR_STORAGE_BUCKET',
+    );
+  }
+}
